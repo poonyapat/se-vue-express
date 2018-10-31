8b1d00f6b2814f60db32737c64ac96de7df58b71
Zyle <poonyapat.y@ku.th>
2018-11-01 06:44:21 +0700

    update layout project tabs

--- client/src/components/ProjectTabs.vue
+++ client/src/components/ProjectTabs.vue
@@ -1,31 +1,33 @@
 <template>
-<div>
-   
-
-<v-toolbar >
-     
-      <v-toolbar-title>{{ project.name }}</v-toolbar-title>
-       <br/> <br/>{{ project.description }}  <!-- รอแก้!!!! -->
-   
-      <v-spacer></v-spacer>
-
-      <v-tabs
-        slot="extension"
-        v-model="tab"
-        dark
-        grow
-      >
-        <!-- <v-tabs-slider color="dark"></v-tabs-slider> -->
-
-        <v-tab
-          v-for="item in items"
-          :key="item"
-        >
-          {{ item }}
-        </v-tab>
-      </v-tabs>
-    </v-toolbar>
-</div>
+  <v-container class="primary" fill-height>
+    <v-layout row wrap>
+      <v-flex>
+        <v-card>
+          <v-toolbar flat>
+            <v-toolbar-title>{{ project.name }}</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+          {{ project.description }} 
+          </v-card-text>
+        </v-card>
+        <v-tabs
+            class="mt-2"
+            slot="extension"
+            v-model="tab"
+            dark
+            grow
+          >
+            <!-- <v-tabs-slider color="dark"></v-tabs-slider> -->
+            <v-tab
+              v-for="item in items"
+              :key="item"
+            >
+              {{ item }}
+            </v-tab>
+        </v-tabs>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
