da4b832211bafee5c37ac017db360ad14234b80c
Zyle <poonyapat.y@ku.th>
2018-11-22 23:09:23 +0700

    fix member table but not finish

--- client/src/components/project/MemberTable.vue
+++ client/src/components/project/MemberTable.vue
@@ -2,8 +2,7 @@
     <div>
         <v-data-table :headers="headers" :items="project.members" hide-actions class="elevation-1">
             <template slot="items" slot-scope="props">
-                <td> {{props.item}}
-                </td>
+                <td> {{props.item}}</td>
                 <td class="text-xs-right">
                     <confirm-dialog @confirm="remove(props.item)" :title="confirm.deletion.title" :text="confirm.deletion.text">
                         <v-btn icon v-if="project.username == username">
@@ -13,17 +12,14 @@
                 </td>
             </template>
         </v-data-table>
-        <v-form>
-            <v-container>
-                <v-layout row wrap>
-                    <v-text-field solo v-model="input.user" label="Add user" type="text" clearable></v-text-field>
-                    <v-btn @click="add()">
-                        <v-icon>person_add</v-icon>
-                    </v-btn>
-                    <snackbar @showBar="showSnackbar" :show="msg" :text="errorMessage.addition.text" />
-                </v-layout>
-            </v-container>
-        </v-form>
+        <v-card class="ma-3">
+            <v-layout row class="pa-2">
+                <v-autocomplete v-model="input.user" :items="suggestion" item-text="name" label="Add Member" clearable></v-autocomplete>
+                <v-btn @click="add()" :dark="!!input.user" :disabled="!input.user">
+                    <v-icon>person_add</v-icon>
+                </v-btn>
+            </v-layout>
+        </v-card>
     </div>
 </template>
 <script>
@@ -47,19 +43,21 @@
                 project: {},
                 headers: [{
                         text: 'Username',
-                        align: 'center',
+                        align: 'left',
                         sortable: false,
                         value: 'name'
                     },
                     {
-                        text: 'Position',
+                        text: '',
                         align: 'center',
+                        sortable: false,
                         value: 'description'
                     },
                 ],
+                suggestion: ['poonyapat'],
                 input: {
                     user: '',
-                    projectId: null,
+                    projectId: null
                 },
                 sender: {
                     user: '',
@@ -67,61 +65,43 @@
                 },
                 confirm: {
                     deletion: {
-                        title: 'Confirm Delete Member',
-                        text: 'Please check its member, when you sure please click confirm'
-                    }
+                        title: 'Confirm Remove Member',
+                        text: 'Please check member before removing them, it may affect to other assignment.<br> when you sure please click confirm'
+                    },
                 },
-                msg: false, //true = have massage error
-                errorMessage: {
-                    addition: {
-                        text: 'Please check  member it have exist, reduntdant name and this name it not member'
-                    }
-                }
             }
         },
         computed: {
-            ...mapState(['username', ])
+            ...mapState(['username', 'route'])
         },
         async mounted() {
-            const id = this.$store.state.route.params.id
+            const id = this.route.params.id
             this.project = (await ProjectService.findOne(id)).data
-            this.input.projectId = this.project.id
         },
         methods: {
             refresh: async function () {
-                const id = this.$store.state.route.params.id
+                const id = this.route.params.id
                 this.project = (await ProjectService.findOne(id)).data
             },
             async add() {
                 try {
                     if (this.input.user != '') {
-                        this.input.projectId = this.project.id;
+                        this.input.projectId = this.project.id
                         await ProjectService.addMember(this.input);
                         this.input.user = '';
                         this.refresh();
-
                     }
                 } catch (error) {
-                    this.msg = true;
                     this.error = error;
                 }
             },
             async remove(memberName) {
                 this.sender.user = memberName;
-                this.sender.projectId = this.project.id;
+                this.sender.projectId = this.project.id
                 await ProjectService.removeMember(this.sender);
                 this.refresh();
             },
-            showSnackbar: function (tmp) {
-                this.msg = tmp;
-            },
         },
-        watch: {
-            msg(value) {
-                console.log("in snack bar (out)")
-                console.log(value);
-            }
-        }
     }
 </script>
 <style></style>
